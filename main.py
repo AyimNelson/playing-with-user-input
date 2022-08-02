180ac2d1da5b6bfd75f0823e3ba05d14474d5e51
Ayim Nelson <ayimnelson01@gmail.com>
2022-08-02 11:34:07 +0000

    Added main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+location = input("Where are you from? ")
+yes = {'yes','y', 'ye', ''}
+no = {'no','n'}
+
+choice = input("Do you like programming? ").lower()
+if choice in yes:
+    print("You like programming")
+elif choice in no:
+       print("You don't like programming")
+else:
+   sys.stdout.write("Please respond with 'yes' or 'no'")
+
+language = input("Do you speak English? ").lower()
+if language in yes:
+    print("You speak English")
+elif language in no:
+       print("You don't speak English")
+else:
+   sys.stdout.write("Please respond with 'yes' or 'no'")
+
+
+print((f"Your name is {name}. \nYou are {age} years old. \nYou are located in {location}. \n"))
